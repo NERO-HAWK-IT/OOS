969c131b639f582e4046f996b8f473f507b770d6
NERO_HAWK <nerohawkit@gmail.com>
2024-02-02 17:14:37 +0300

    доп

--- ecology/views.py
+++ ecology/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import *
-from .work import stok
+from .work import *
 
 
 def test(request):
     waste = Waste_types.actual.all()
     waste_stok = Waste_types.objects.filter(code='1471501')
+    data = get_stok('Е.А.Богомья')
     context = {
         'title': 'Экология',
         'waste': waste,
         'wa': waste_stok,
+        'data': data,
     }
     return render(request, 'ecology/ecology.html', context=context)
