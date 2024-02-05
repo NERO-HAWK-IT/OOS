0a1b77ecb35366acc0dcdcf666f336cb3a95bc7f
NERO_HAWK <nerohawkit@gmail.com>
2024-02-05 19:22:36 +0300

    комент на data

--- ecology/views.py
+++ ecology/views.py
@@ -7,7 +7,7 @@ from .work import *
 def test(request):
     waste = Waste_types.actual.all()
     waste_stok = Waste_types.objects.filter(code='1471501')
-    data = get_stok('Е.А.Богомья')
+    # data = get_stok('Е.А.Богомья')
     context = {
         'title': 'Экология',
         'waste': waste,
